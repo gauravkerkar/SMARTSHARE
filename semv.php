39365a6a0c721c285bac909f494bd46fecae89f1
Namav404 <sec.acc901@gmail.com>
2019-03-09 12:07:38 +0530

    a

---
+++ semv.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SMARTSHARE | SEM V</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Bootstrap 4 link-->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/jquery/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<style>
+    body,
+    html {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        background: url(img/background.svg) !important;
+    }
+
+    .sem5 {
+        padding: 100px;
+        text-align: center;
+
+    }
+</style>
+
+<body>
+
+    <div class="sem5">
+        <h1 class="text-center text-white pb-5" style="font-family:'Times New Roman', Times, serif">SEM V</h1>
+        <div class="row">
+            <div class="col"><a href="notes.html" data-toggle="tooltip" title="Notes"><img src="img/notes.png"></a>
+            </div>
+            <div class="col"><a href="notice.html" data-toggle="tooltip" title="Notice"><img src="img/notice.png"></a>
+            </div>
+            <div class="col"><a href="timetable.html" data-toggle="tooltip" title="TimeTable"><img
+                        src="img/timetable.png"></a></div>
+            <div class="col"><a href="question.html" data-toggle="tooltip" title="Question"><img
+                        src="img/question.png"></a></div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ server.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+// define variables and set to empty values
+$en_no = $name = $email = $mob_no = $username = $password = "";
+
+// Connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'smartshare');
+
+// If the register button is clicked
+if (isset($_POST['register'])) {
+    $en_no = mysqli_real_escape_string($db, $_POST['en_no']);
+    $name = mysqli_real_escape_string($db, $_POST['name']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $mob_no = mysqli_real_escape_string($db, $_POST['mob_no']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  // If no errors, save the user's data to the database
+  if ($en_no != "" && $name != "" && $email != "" && $mob_no != "" && $password != "") {
+    $query = "INSERT INTO students (en_no, name, email, phone_no, password) VALUES ('$en_no', '$name', '$email', '$mob_no', '$password')";
+    mysqli_query($db, $query);
+    header('location: login_student.php'); //redirect to login page
+    exit();	
+  }
+}
+
+// Login user
+if (isset($_POST['login'])) {
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if ($email != "" && $password != "") {
+    $query = "SELECT en_no FROM students WHERE email='$email' AND password='$password'";
+    $result = mysqli_query($db, $query);
+    if (mysqli_num_rows($result)==1) {
+      $_SESSION['student'] = $email;
+      header('location: index.php'); //redirect to index page
+      exit();	
+    } else {
+       echo '<script>alert("Invalid email and password!")</script>';
+    }
+  }
+}
+
+  // Admin Login user
+if (isset($_POST['admin_login'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if ($username != "" && $password != "") {
+    $query = "SELECT id FROM admin WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($db, $query);
+    if (mysqli_num_rows($result)==1) {
+        $_SESSION['admin'] = $username;
+        header('location: homepage.php'); //redirect to homepage page
+        exit();	
+    } else {
+        echo '<script>alert("Invalid username and password!")</script>';
+    }
+  }
+}
+
+?>
