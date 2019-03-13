dc4fbf544cb7f1a0b28467f93ef98232bb1b8989
gauravkerkar <gauravkerkar1999@gmail.com>
2019-03-13 12:00:51 +0530

    nklkj

---
+++ admin/login.php
@@ -0,0 +1,178 @@
+<?php
+session_start();
+// Connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'smartshare');
+// Admin Login user
+if (isset($_POST['admin_login'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if ($username != "" && $password != "") {
+    $query = "SELECT id FROM admin WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($db, $query);
+    if (mysqli_num_rows($result)==1) {
+        $_SESSION['admin'] = $username;
+        header('location: index.php'); //redirect to homepage page
+        exit();	
+    } else {
+        echo '<script>alert("Invalid username and password!")</script>';
+    }
+  }
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SMARTSHARE | ADMIN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Bootstrap 4 link-->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <script src="../bootstrap/jquery/jquery.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+
+    <script>
+        function showpass() {
+            var pass = document.getElementById('pass');
+            if (document.getElementById('customControlInline').checked) {
+                pass.setAttribute('type', 'text');
+            } else {
+                pass.setAttribute('type', 'password');
+            }
+        }
+    </script>
+
+    <style>
+        body,
+        html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            background: url(../img/background.svg) !important;
+        }
+
+        .user_card {
+            height: 400px;
+            width: 350px;
+            margin-top: auto;
+            margin-bottom: auto;
+            background: orchid;
+            position: relative;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            padding: 10px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            -webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            -moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            border-radius: 5px;
+
+        }
+
+        .brand_logo_container {
+            position: absolute;
+            height: 170px;
+            width: 170px;
+            top: -75px;
+            border-radius: 50%;
+            background: whitesmoke;
+            padding: 10px;
+            text-align: center;
+        }
+
+        .brand_logo {
+            height: 150px;
+            width: 150px;
+            border-radius: 50%;
+            border: 2px solid white;
+        }
+
+        .form_container {
+            margin-top: 100px;
+        }
+
+        .login_btn {
+            width: 100%;
+            background: rgb(132, 0, 100) !important;
+            color: white !important;
+        }
+
+        .login_btn:focus {
+            box-shadow: none !important;
+            outline: 0px !important;
+        }
+
+        .login_container {
+            padding: 0 2rem;
+        }
+
+        .input-group-text {
+            background: rgb(132, 0, 100) !important;
+            color: white !important;
+            border: 0 !important;
+            border-radius: 0.25rem 0 0 0.25rem !important;
+            padding: 6px;
+        }
+
+        .input_user,
+        .input_pass:focus {
+            box-shadow: none !important;
+            outline: 0px !important;
+        }
+
+        .custom-checkbox .custom-control-input:checked~.custom-control-label::before {
+            background-color: #c0392b !important;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container h-100">
+        <div class="d-flex justify-content-center h-100">
+            <div class="user_card">
+                <div class="d-flex justify-content-center">
+                    <div class="brand_logo_container">
+                        <img src="../img/logo.png" class="brand_logo" alt="Logo">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center form_container">
+                    <form method="POST">
+                        <div class="input-group mb-3">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><img src="../img/user_icon.PNG" width="24"></span>
+                            </div>
+                            <input type="text" name="username" class="form-control input_user" placeholder="Admin" required>
+                        </div>
+                        <div class="input-group mb-2">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><img src="../img/lock_icon.PNG" width="24"></span>
+                            </div>
+                            <input type="password" name="password" class="form-control input_pass" id="pass"
+                                placeholder="Password" required>
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" onclick="showpass();"
+                                    id="customControlInline">
+                                <label class="custom-control-label" for="customControlInline">Show Password</label>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center mt-3 login_container">
+                            <input type="submit" name="admin_login" class="btn login_btn" value="Login">
+                        </div>
+                    </form>
+                </div>
+                <div class="d-flex justify-content-center links">
+                    <a href="forgot_password.php" class="text-white">Forgot your password?</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</body>
+
+</html>
---
+++ student/index.php
@@ -0,0 +1,109 @@
+<?php include('session.php'); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SMARTSHARE | INDEX</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Bootstrap 4 link-->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <script src="../bootstrap/jquery/jquery.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            background: url(../img/background.svg) !important;
+            background-size: contain;
+        }
+
+        a.buttonsborder:hover {
+            color: #000;
+            border: 1px solid #0066fff1;
+        }
+
+        .btn-app {
+            border-radius: 3px;
+            position: relative;
+            padding: 6px 22px;
+            margin: 0 0 10px 10px;
+            min-width: 121px;
+            height: 80px;
+            text-align: center;
+            border: 1px solid #ddd;
+            color: #000;
+            background-color: #f4f4f4;
+            font-size: 1rem;
+        }
+
+        .btn-app .sem {
+            display: block;
+            width: 65px;
+            height: 48px;
+            color: rgb(130, 0, 120);
+        }
+
+        .box-hero {
+            position: relative;
+            border-radius: 8px;
+            margin-top: 30px;
+            background: #ffffff;
+            border-top: 3px solid #3c8dbc;
+            margin-bottom: 20px;
+
+            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+        }
+
+        .margin {
+            margin-top: 30px;
+        }
+
+        div.dropdown-menu.p-2 {
+            width: max-content;
+        }
+        div .sam{
+            width: 158px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <?php include('navigation.php'); ?>
+    <div class="container box-hero">
+        <h4 class="box-body">Quick Links</h4>
+        <p class="margin"></p>
+        <div class="row">
+            <div class="col-xs-6 col-md-2 sam">
+                <a class="btn btn-app buttonsborder" href=""><img src="../img/sem1.png" class="sem"
+                        alt="sem1">Semester I</a>
+            </div>
+            <div class="col-xs-6 col-md-2 sam">
+                <a class="btn btn-app buttonsborder" href=""><img src="../img/sem2.png" class="sem"
+                        alt="sem2">Semester
+                    II</a>
+            </div>
+            <div class="col-xs-6 col-md-2 sam">
+                <a class="btn btn-app buttonsborder" href=""><img src="../img/sem3.png" class="sem"
+                        alt="sem3">Semester
+                    III</a>
+            </div>
+            <div class="col-xs-6 col-md-2 sam">
+                <a class="btn btn-app buttonsborder" href=""><img src="../img/sem4.png" class="sem"
+                        alt="sem4">Semester
+                    IV</a>
+            </div>
+            <div class="col-xs-6 col-md-2 sam">
+                <a class="btn btn-app  buttonsborder" href=""><img src="../img/sem5.png" class="sem"
+                        alt="sem5">Semester V</a>
+            </div>
+            <div class="col-xs-6 col-md-2 sam">
+                <a class="btn btn-app buttonsborder" href=""><img src="../img/sem6.png" class="sem"
+                        alt="sem6">Semester VI</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
