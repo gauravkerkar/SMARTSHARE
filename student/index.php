fb17218a3b7ee076cc3a7bf6ec58ddcb4502c917
Namav404 <sec.acc901@gmail.com>
2019-03-17 23:25:33 +0530

    a

--- student/index.php
+++ student/index.php
@@ -72,37 +72,26 @@
             width: 158px;
         }
         #img_div{
-   	width: 80%;
-   	padding: 5px;
-   	margin: 15px auto;
-   	border: 1px solid #cbcbcb;
-   }
-   #img_div:after{
-   	content: "";
-   	display: block;
-   	clear: both;
-   }
-   #image-id{
-   	float: left;
-   	margin: 5px;
-   	width: 300px;
-   	height: 140px;
-   }
-   #content{
-   	width: 50%;
-   	margin: 20px auto;
-   	border: 1px solid #cbcbcb;
-       background: rgba(233, 201, 57, 0.425);
-   }
-   .notice{
-       background: rgba(17, 17, 17, 0.24);
-       border-top: 7px solid rgb(69, 150, 243);
-       border-radius:10px;
-       border-bottom: 7px solid rgb(56, 20, 255); 
-   }
-   h3{
-       border-bottom: 2px solid whitesmoke;
-   }
+        	width: 80%;
+        	padding: 5px;
+        	margin: 15px auto;
+        	border: 1px solid #cbcbcb;
+        }
+        #content{
+        	width: 50%;
+        	margin: 20px auto;
+        	border: 1px solid #cbcbcb;
+            background: rgba(233, 201, 57, 0.425);
+        }
+        .notice{
+            background: rgba(17, 17, 17, 0.24);
+            border-top: 7px solid rgb(69, 150, 243);
+            border-radius:10px;
+            border-bottom: 7px solid rgb(56, 20, 255); 
+        }
+        h3{
+            border-bottom: 2px solid whitesmoke;
+        }
     </style>
 </head>
 
@@ -146,8 +135,8 @@
             <?php
               while ($row = mysqli_fetch_array($result)) {
                 echo "<div id='img_div'>";
-                    echo "<a href='../notice/".$row['image']."' download><img id='image-id' class='img-fluid' src='../notice/".$row['image']."' ></a>";
-                    echo "<p>".$row['image_txt']."</p>";
+                    echo "<a href='../notice/".$row['image']."' download><img class='img-fluid rounded' src='../notice/".$row['image']."' ></a>";
+                    echo "<h4 class='text-center text-white'>".$row['image_txt']."</h4>";
                 echo "</div>";
               }
             ?>
